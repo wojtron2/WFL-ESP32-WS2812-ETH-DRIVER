eaf340a3277695577766a7085243099b99fea1f2
wojtron <67229366+wojtron2@users.noreply.github.com>
2024-10-06 05:08:45 +0200

    added temp measurement

--- esp32_eth_w5500_webserver7_ws2812v1/esp32_eth_w5500_webserver7_ws2812v1.ino
+++ esp32_eth_w5500_webserver7_ws2812v1/esp32_eth_w5500_webserver7_ws2812v1.ino
@@ -2,13 +2,17 @@
 #include <Arduino.h>
 #include <SPI.h>
 #include <Ethernet.h>
-#include <ArduinoJson.h> // Dodaj bibliotekę ArduinoJson do obsługi JSON
+#include <ArduinoJson.h> // Dodaje biblioteke ArduinoJson do obsługi JSON
+
+#include <OneWire.h>
+#include <DallasTemperature.h>
 
 #define LEDS_COUNT 1
 #define LEDS_PIN 27
 #define CHANNEL 0
 
 Freenove_ESP32_WS2812 ledstrip = Freenove_ESP32_WS2812(LEDS_COUNT, LEDS_PIN, CHANNEL, TYPE_GRB);
+//Adafruit_NeoPixel ledstrip(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
 byte mac[] = { 0xFE, 0xFE, 0xBE, 0xFE, 0xDE, 0xED };
 IPAddress ip(192, 168, 2, 177);
@@ -28,6 +32,18 @@ unsigned long currentTime = millis();
 unsigned long previousTime = 0;
 const long timeoutTime = 2000;
 
+// temp meas
+// GPIO where the DS18B20 is connected to
+const int oneWireBus = 4;     
+
+// Setup a oneWire instance to communicate with any OneWire devices
+OneWire oneWire(oneWireBus);
+
+// Pass our oneWire reference to Dallas Temperature sensor 
+DallasTemperature sensors(&oneWire);
+
+// ## temp meas
+
 void setup() {
   pinMode(output26, OUTPUT);
   pinMode(output25, OUTPUT);
@@ -38,6 +54,7 @@ void setup() {
   ledstrip.begin();
   ledstrip.setBrightness(100);
   ledstrip.setLedColorData(0, 0, 0, 0);
+ // ledstrip.clear(); 
   ledstrip.show();
 
   Serial.begin(115200);
@@ -57,6 +74,10 @@ void setup() {
   server.begin();
   Serial.print("server is at ");
   Serial.println(Ethernet.localIP());
+  ledstrip.setLedColorData(0, 255, 0, 0);
+  ledstrip.show();
+
+  sensors.begin();
 }
 
 void loop() {
@@ -100,13 +121,19 @@ void loop() {
               currentBlue = blue;
               LEDState = "on";
               ledstrip.show();
-            } else if (header.indexOf("GET /LEDs/brightness/on") >= 0) {
-              Serial.println("LED on");
+            } else if (header.indexOf("GET /LEDs/brightness/30") >= 0) {
+              Serial.println("brightness 30");
+              ledstrip.setBrightness(30);
+              LEDState = "on";
+              ledstrip.show();
+            }
+              else if (header.indexOf("GET /LEDs/brightness/on") >= 0) {
+              Serial.println("brightness 100");
               ledstrip.setBrightness(100);
               LEDState = "on";
               ledstrip.show();
             } else if (header.indexOf("GET /LEDs/brightness/off") >= 0) {
-              Serial.println("LED off");
+              Serial.println("brightness 0");
               ledstrip.setBrightness(0);
               LEDState = "off";
               ledstrip.show();
@@ -122,7 +149,58 @@ void loop() {
               currentBlue = blue;
               LEDState = "on";
               ledstrip.show();
-            } else if (header.indexOf("GET /RGB/r255g0b0") >= 0) {
+            }
+              else if (header.indexOf("GET /RGBall/r") >= 0) {
+              // Extract RGB values from URL
+              int red = header.substring(header.indexOf("r") + 1, header.indexOf("g")).toInt();
+              int green = header.substring(header.indexOf("g") + 1, header.indexOf("b")).toInt();
+              int blue = header.substring(header.indexOf("b") + 1).toInt();
+              //Serial.printf("Received color - R:%d G:%d B:%d\n", red, green, blue);
+              ledstrip.setAllLedsColorData(red, green, blue);
+              currentRed = red;
+              currentGreen = green;
+              currentBlue = blue;
+              LEDState = "on";
+              ledstrip.show();
+            } 
+              else if (header.indexOf("GET /RGBall/red") >= 0) {
+              Serial.println("Set color to Red all");
+              ledstrip.setAllLedsColorData(255, 0, 0);
+              currentRed = 255;
+              currentGreen = 0;
+              currentBlue = 0;
+              LEDState = "on";
+              ledstrip.show();
+            } 
+              else if (header.indexOf("GET /RGBall/green") >= 0) {
+              Serial.println("Set color to Green all");
+              ledstrip.setAllLedsColorData(0, 255, 0);
+              currentRed = 255;
+              currentGreen = 0;
+              currentBlue = 0;
+              LEDState = "on";
+              ledstrip.show();
+            } 
+              else if (header.indexOf("GET /RGBall/blue") >= 0) {
+              Serial.println("Set color to Blue all");
+              ledstrip.setAllLedsColorData(0, 0, 255);
+              currentRed = 255;
+              currentGreen = 0;
+              currentBlue = 0;
+              LEDState = "on";
+              ledstrip.show();
+            } 
+            else if (header.indexOf("GET /RGBall/pink") >= 0) {
+              Serial.println("Set color to Blue all");
+              ledstrip.setAllLedsColorData(255, 0, 125);
+              currentRed = 255;
+              currentGreen = 0;
+              currentBlue = 0;
+              LEDState = "on";
+              ledstrip.show();
+            } 
+            
+              else if (header.indexOf("GET /RGB/r255g0b0") >= 0) {
               Serial.println("Set color to Red");
               ledstrip.setLedColorData(0, 255, 0, 0);
               currentRed = 255;
@@ -163,6 +241,12 @@ void loop() {
               LEDState = "on";
               ledstrip.show();
             }
+            else if (header.indexOf("GET /temp") >= 0) {
+              Serial.println("GET TEMP");
+
+            get_temp_to_http();
+
+            }
 
             // Display the HTML web page
             client.println("<!DOCTYPE html><html>");
@@ -186,15 +270,28 @@ void loop() {
 
             // Display LED state
             client.println("<p>LEDState: " + LEDState + "</p>");
-            client.println("<p><a href=\"/LEDs/brightness/on\"><button class=\"button\">Turn ON LED</button></a></p>");
-            client.println("<p><a href=\"/LEDs/brightness/off\"><button class=\"button button2\">Turn OFF LED</button></a></p>");
+            client.println("<p><a href=\"/LEDs/brightness/on\"><button class=\"button\">Bright 100</button></a></p>");
+            client.println("<p><a href=\"/LEDs/brightness/30\"><button class=\"button\">Bright 30</button></a></p>");
+            client.println("<p><a href=\"/LEDs/brightness/off\"><button class=\"button button2\">Bright 0</button></a></p>");
             
             // Display color control buttons
-            client.println("<p><a href=\"/RGB/r255g0b0\"><button class=\"button\">Red</button></a></p>");
-            client.println("<p><a href=\"/RGB/r0g255b0\"><button class=\"button\">Green</button></a></p>");
-            client.println("<p><a href=\"/RGB/r0g0b255\"><button class=\"button\">Blue</button></a></p>");
+            client.println("<p><a href=\"/RGB/r255g0b0\"><button class=\"button\">Red</button></a><a href=\"/RGBall/red\"><button class=\"button\">Red all</button></a></p>");
+            client.println("<p><a href=\"/RGB/r0g255b0\"><button class=\"button\">Green</button></a><a href=\"/RGBall/green\"><button class=\"button\">Green all</button></a></p>");
+            client.println("<p><a href=\"/RGB/r0g0b255\"><button class=\"button\">Blue</button></a><a href=\"/RGBall/blue\"><button class=\"button\">Blue all</button></a></p>");
             client.println("<p><a href=\"/RGB/r255g255b255\"><button class=\"button\">White</button></a></p>");
-            client.println("<p><a href=\"/RGB/r255g0b125\"><button class=\"button\">Pink</button></a></p>");
+            client.println("<p><a href=\"/RGB/r255g0b125\"><button class=\"button\">Pink</button></a><a href=\"/RGBall/pink\"><button class=\"button\">Pink all</button></a></p>");
+
+            client.println("<p></p> <p></p>");
+            
+          
+            client.println("<p><a href=\"/temp\"><button class=\"button\">Get temp</button></a></p>");
+            /*
+            if (temperatureC =! 0) {
+            //client.println("<p>TEMP IS " temperatureC " C </p>");
+
+            client.println("<p>TEMP IS: " + String(temperatureC) + "</p>");
+            }
+          */
             
             client.println("</body></html>");
             client.println();
@@ -214,3 +311,29 @@ void loop() {
     Serial.println("");
   }
 }
+
+
+
+
+void get_temp_to_http(void){
+  sensors.requestTemperatures(); 
+  float temperatureC = sensors.getTempCByIndex(0);
+
+
+  Serial.print(temperatureC);
+  Serial.println("ºC");
+}
+
+float get_temp_to_http_ext(void){
+  sensors.requestTemperatures(); 
+  float temperatureC2 = sensors.getTempCByIndex(0);
+
+
+  return temperatureC2;
+}
+
+/*
+float get_temp_as_float(void){
+
+}
+*/
---
+++ esp32_eth_w5500_webserver7_ws2812v1/piny.md
@@ -0,0 +1,20 @@
+leds pin 27
+
+spi vspi
+cs 5
+clk 18
+miso 19
+mosi 23
+
+
+//dodajac piny interrupt i reset a conajmniej interrupt mozna osiagnac lepsza reakcje na dane
+
+
+gpio 4 standardowo jako onewire
+mozna np 32, 33
+
+
+clk do 18
+cs do 5
+miso 19
+mosi 23
